a83e5fc54520de77052fff2112fef208724908ed
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-12 08:21:56 -0700

    Create status code test for POST /games

--- server.test.js
+++ server.test.js
@@ -2,5 +2,19 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('Server.js endpoint tests', () => {
-	describe('POST /games', () => {});
+	describe('POST /games', () => {
+		it('should return status 200 OK after creating a game', async () => {
+			const game = {
+				title: 'Pacman',
+				genre: 'Arcade',
+				releaseYear: 1980
+			};
+
+			const response = await request(server)
+				.post('/games')
+				.send(game);
+
+			expect(response).toBe(200);
+		});
+	});
 });
