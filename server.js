f776d0995ffcbafbf4957fedef3b09879dff50fc
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-12 08:41:29 -0700

    Create POST /games endpoint

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ server.get('/games', async (req, res) => {
 	return await res.status(200).json(db);
 });
 
-server.get('');
+server.post('/games', async (req, res) => {
+	let { title, genre } = req.body;
+
+	if (!title || !genre)
+		return res.status(422).json({
+			message: 'Sorry, but you must enter a title and genre'
+		});
+
+	await db.push(req.body);
+
+	return res.status(200).json(req.body);
+});
 
 server.listen(5000, () =>
 	console.log('Server is now up and running at http://localhost:5000')
