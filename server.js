d90b9d1ab0dbccecbaadb6c8d78c189d6b79b45a
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-12 08:38:50 -0700

    Create GET /games endpoint

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const db = [];
 
 server.use(express.json());
 
+server.get('/games', async (req, res) => {
+	return await res.status(200).json(db);
+});
+
+server.get('');
+
 server.listen(5000, () =>
 	console.log('Server is now up and running at http://localhost:5000')
 );
